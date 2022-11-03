057a08744b182a6392f8cd2a5621efe940083221
CrampedAverage <adnan.aant@gmail.com>
2022-11-03 23:03:55 +0000

    Created a file structure for pages and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/register",
+    element: <Register />
+  }
+]);
+
+const App = () =>  {
   return (
-    <div className="App">
-
-      <p className="">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
