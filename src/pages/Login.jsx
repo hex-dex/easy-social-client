878142e5b8b8a7c5b799f3e79e6d27741a6ddb4c
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-04 22:32:35 +0000

    added JSX and a reusable style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className='center'>
+      <div className='mt-60 h-80 w-80 rounded-lg border-2 border-red-900 text-center'>
+        <h1 className='text-2xl mt-10'>Login</h1>
+        <form className='center'>
+          <input
+            type='email'
+            className='bg-custom-blue border-2 border-red-900'
+          />
+          <input type='password' className='border-2 border-red-900' />
+          <button>Login</button>
+          <p>Forgotten Password</p>
+          <button>Register</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
