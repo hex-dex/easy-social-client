c7503ac994827815f24efb5a0970e95b42f61ed0
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-11-05 20:06:18 +0000

    added more styles and added labels

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
+import background from '../assets/sky.jpg';
 
 const Login = () => {
   return (
-    <div className='center'>
-      <div className='mt-60 h-80 w-80 rounded-lg border-2 border-red-900 text-center'>
-        <h1 className='text-2xl mt-10'>Login</h1>
-        <form className='center'>
-          <input
-            type='email'
-            className='bg-custom-blue border-2 border-red-900'
-          />
-          <input type='password' className='border-2 border-red-900' />
-          <button>Login</button>
-          <p>Forgotten Password</p>
-          <button>Register</button>
+    <div
+      className='center h-screen bg-no-repret bg-cover'
+      style={{ backgroundImage: `url(${background})` }}
+    >
+      <div className='mt-60 h-96 w-96 rounded-lg bg-black bg-opacity-20'>
+        <h1 className='text-2xl mt-10 mb-3 text-hawkes-blue-500 text-center'>
+          Login
+        </h1>
+        <form className='flex flex-col'>
+          <label className=' ml-20 mt-3 text-sm text-hawkes-blue-500'>
+            Username
+          </label>
+          <div className='center'>
+            <input type='email' className='forms mt-0.5' />
+          </div>
+          <label className=' ml-20 mt-3 text-sm text-hawkes-blue-500'>
+            Password
+          </label>
+          <div className='center'>
+            <input type='password' className='forms mt-0.5 ' />
+          </div>
+          <div className='center'>
+            <button className='btn mt-6 '>Login</button>
+            <p className='mt-7 text-xs mb-1 text-hawkes-blue-500'>
+              Forgotten Password
+            </p>
+            <button className='btn'>Register</button>
+          </div>
         </form>
       </div>
     </div>
