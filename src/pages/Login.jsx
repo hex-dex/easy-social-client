010195c04acea9b35c3d212604f534e9ac67ec58
CrampedAverage <adnan.aant@gmail.com>
2022-11-10 23:02:34 +0000

    Added prettier

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,21 +11,21 @@ const Login = () => {
   const onSubmit = (values) => console.log(values);
   return (
     <div
-      className='center h-screen bg-no-repret bg-cover'
+      className="center h-screen bg-no-repret bg-cover"
       style={{ backgroundImage: `url(${background})` }}
     >
-      <div className='my-auto p-4 w-96 rounded-lg bg-black bg-opacity-20'>
-        <h1 className='text-2xl mt-5 mb-3 text-hawkes-blue-500 text-center'>
+      <div className="my-auto p-4 w-96 rounded-lg bg-black bg-opacity-20">
+        <h1 className="text-2xl mt-5 mb-3 text-hawkes-blue-500 text-center">
           Login
         </h1>
-        <form className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
-          <label className=' ml-20 mt-3 text-sm text-hawkes-blue-500'>
+        <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}>
+          <label className=" ml-20 mt-3 text-sm text-hawkes-blue-500">
             Username
           </label>
-          <div className='center'>
+          <div className="center">
             <input
-              type='text'
-              className='forms mt-0.5'
+              type="text"
+              className="forms mt-0.5"
               {...register('text', {
                 required: 'Enter Username',
                 pattern: {
@@ -33,18 +33,18 @@ const Login = () => {
                 },
               })}
             />
-            <span className='text-red-700'>
+            <span className="text-red-700">
               {errors?.text && errors.text.message}
             </span>
           </div>
-          <label className=' ml-20 mt-3 text-sm text-hawkes-blue-500'>
+          <label className=" ml-20 mt-3 text-sm text-hawkes-blue-500">
             Password
           </label>
-          <div className='center'>
+          <div className="center">
             <input
-              type='password'
-              name='password'
-              className='forms mt-0.5'
+              type="password"
+              name="password"
+              className="forms mt-0.5"
               {...register('password', {
                 required: 'Required',
                 pattern: {
@@ -52,16 +52,16 @@ const Login = () => {
                 },
               })}
             />
-            <span className='text-red-700'>
+            <span className="text-red-700">
               {errors?.password && errors.password.message}
             </span>
           </div>
-          <div className='center'>
-            <button className='btn mt-6 '>Login</button>
-            <p className='mt-5 text-xs mb-1 text-hawkes-blue-500'>
+          <div className="center">
+            <button className="btn mt-6 ">Login</button>
+            <p className="mt-5 text-xs mb-1 text-hawkes-blue-500">
               Forgotten Password
             </p>
-            <button className='btn'>Register</button>
+            <button className="btn">Register</button>
           </div>
         </form>
       </div>
