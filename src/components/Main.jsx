dfd390689bb9700958009c9b4080d2d7263418f3
Killdroid2342 <113387485+Killdroid2342@users.noreply.github.com>
2022-12-05 20:40:41 +0000

    finished posts

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import usePosts from '../hooks/usePosts';
 import CreatePost from './CreatePost';
+import Post from './Post';
 
 const MainSelection = () => {
   return (
@@ -10,18 +12,28 @@ const MainSelection = () => {
   );
 };
 
-const Main = () => {
+const Main = ({ user }) => {
+  const { posts, createPost } = usePosts(user);
   return (
     <div
       className="h-full flex flex-col flex-shrink-0 mx-2 max-w-screen max-md:flex-shrink"
       style={{ width: '28rem' }}
     >
-      <CreatePost />
+      <CreatePost createPost={createPost} user={user} />
       <MainSelection />
-      <div className="bg-container flex-auto rounded-2xl p-4">
+      <div className="bg-container flex-auto rounded-2xl p-4 overflow-y-auto">
         <h1 className="text-primary mx-auto w-fit mb-4 text-2xl tracking-wide font-bold">
           Posts
         </h1>
+        {posts.map((post) => {
+          return (
+            <Post
+              post_id={post.post_id}
+              source_id={post.source_id}
+              post_content={post.post_content}
+            />
+          );
+        })}
       </div>
     </div>
   );
